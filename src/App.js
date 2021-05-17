d0d1046a10bdf98ad97d400f4373ee943c02b366
andryue <kacperwojak17@gmail.com>
2021-05-17 22:14:06 +0200

    add responsive header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import './App.css';
-import Header from './components/Header/Header';
+import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div>
-      <Header/>
+      <Header />
+      fojwoefjo jf ow o<br/>
+      f<br/>
+       wd<br/>
+       f wd<br/>
+       f <br/>
+       wd<br/>
+       f we<br/>
+       f w<br/>
+       edf <br/>
+       we<br/>
+        w<br/>
+        we
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,50 @@
- import React from 'react'
- import styles from './Header.module.css'
+import { AppBar, Toolbar, IconButton } from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+import React, { useState } from "react";
+import styles from "./Header.module.css";
+import SideDrawer from "./SideDrawer";
 
- const Header = () =>{
+const Header = () => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
-    return (
-    <div className={styles.header}>
-        Siemanko to ja
+  const toggleOpenDrawer = (isOpen) => (event) => {
+    if (
+      event &&
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+    setIsDrawerOpen(isOpen);
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <AppBar className={styles.mobileHeader} style={{ background: "#ffffff" }}>
+        <Toolbar>
+          <IconButton
+            edge="start"
+            aria-label="menu"
+            onClick={toggleOpenDrawer(true)}
+          >
+            <MenuIcon className={styles.menuIcon} />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <SideDrawer toggleDrawer={(value) => toggleOpenDrawer(value)} isDrawerOpen={isDrawerOpen} />
+      <header className={styles.header}>
+        <span>HOME</span>
+        <span>PORTFOLIO</span>
+        <span>RESUME</span>
+        <span>ABOUT</span>
+        <span>CONTACT</span>
+      </header>
+      <div className={styles.titles}>
+        <span className={styles.helloText}>Hello, I'm</span>
+        <span className={styles.nameText}>Kacper Wojak</span>
+        <span className={styles.portfolioText}>AND THIS IS MY POTFOLIO</span>
+      </div>
     </div>
-    )
- }
- export default Header
+  );
+};
+export default Header;
