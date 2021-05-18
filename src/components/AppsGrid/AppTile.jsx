3e40281980e3cf8b6dc902b0ea55d5adc6308f9f
andryue <kacperwojak17@gmail.com>
2021-05-18 18:16:28 +0200

    finish app tile

--- src/components/AppsGrid/AppTile.jsx
+++ src/components/AppsGrid/AppTile.jsx
@@ -1,17 +1,25 @@
-import React, { useRef } from "react";
+import React, { useState } from "react";
 import styles from "./AppTile.module.css";
-import { Button } from "@material-ui/core";
 
 const AppTile = (props) => {
+  const [isHover, setIsHover] = useState(false);
+
   return (
     <div className={styles.allTile}>
-      <div className={styles.appTileWrapper}>
+      <div
+        className={styles.appTileWrapper}
+        onMouseOver={() => setIsHover(true)}
+        onMouseOut={() => setIsHover(false)}
+      >
+        <div className={!isHover? styles.shadow : styles.shadowHover}></div>
         <img
           src="https://cdn.pixabay.com/photo/2020/08/01/12/18/winnats-pass-5455266_960_720.jpg"
           className={styles.image}
           alt="App"
         />
-        <div className={styles.button}>Kliklniej mnie</div>
+        <button className={!isHover ? styles.button : styles.buttonHover}>
+          Check this out
+        </button>
       </div>
       <div className={styles.appName}>Imagine</div>
       <div className={styles.appYear}>2020</div>
